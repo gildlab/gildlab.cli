4b6f38d5e98b6a7292d3bbaaaa4f776d449bb7cc
thedavidmeister <thedavidmeister@gmail.com>
2023-04-25 12:43:03 +0400

    dedupe pins

--- src/cli/pins.rs
+++ src/cli/pins.rs
@@ -4,6 +4,7 @@ use crate::subgraph::Subgraph;
 use clap::ArgMatches;
 use strum::IntoEnumIterator;
 use crate::author::AUTHORS;
+use std::collections::HashSet;
 
 pub static NAME: &str = "pins";
 pub static ABOUT: &str = "Fetches all pins from all authors from all known subgraphs.";
@@ -14,7 +15,7 @@ pub async fn pins(_matches: &ArgMatches) -> anyhow::Result<()> {
 
     let tasks = Network::iter()
         .map(|network| crate::subgraph::pin::pins_from_subgraph(Subgraph { network }, authors.clone()));
-    let pins: Vec<IPFSCID> = futures::future::join_all(tasks)
+    let pins: HashSet<IPFSCID> = futures::future::join_all(tasks)
         .await
         .into_iter()
         .collect::<anyhow::Result<Vec<Vec<IPFSCID>>>>()?
@@ -22,5 +23,8 @@ pub async fn pins(_matches: &ArgMatches) -> anyhow::Result<()> {
         .flatten()
         .collect();
     dbg!(pins.len());
+    for pin in pins {
+        println!("{}", bs58::encode(pin.to_bytes()).into_string());
+    }
     Ok(())
 }
