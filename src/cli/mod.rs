4a41332f49b7a82dea2f8fa08a3ff838887e3142
thedavidmeister <thedavidmeister@gmail.com>
2023-04-25 18:32:39 +0400

    remove debug

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -4,7 +4,7 @@ use clap::Command;
 mod pins;
 
 pub async fn main() -> Result<()> {
-    tracing::subscriber::set_global_default(tracing_subscriber::fmt::Subscriber::new())?;
+    // tracing::subscriber::set_global_default(tracing_subscriber::fmt::Subscriber::new())?;
 
     let matches = command!()
         .subcommand(Command::new(pins::NAME).about(pins::ABOUT))
