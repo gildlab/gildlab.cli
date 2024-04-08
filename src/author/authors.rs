18b61ffb243095b1a57149d0dd2c58d54950e17e
ninokeldishvili <ninokeldishvili@gmail.com>
2024-04-08 12:43:38 +0400

    Remove unused cbor_decode

--- src/author/authors.rs
+++ src/author/authors.rs
@@ -39,13 +39,6 @@ async fn get_data(url: &str, query: &str) -> Result<serde_json::Value> {
     Ok(data)
 }
 
-fn cbor_decode(encoded_str: &str) -> Result<Vec<u8>>{
-    let extracted_substring = &encoded_str[18..]; //Remove rain meta magic_number
-    let encoded = hex::decode(extracted_substring).expect("Error decoding hex string");
-    let decoded: Payload = from_slice(&encoded)?;
-
-   Ok(encoded)
-}
 
 pub async fn get_authors() -> Result<Vec<String>> {
     let query = r#"
@@ -57,24 +50,27 @@ pub async fn get_authors() -> Result<Vec<String>> {
     "#;
 
     let url = "https://api.thegraph.com/subgraphs/name/ninokeldishvili/rain-metaboard";
-    let res = get_data(url, query).await?;
+//     let res = get_data(url, query).await?;
 
 
-//   let res = json!({
-//         "data": {
-//             "metaV1S": [
-//                 {
-//                     "meta": "0xff0a89c674ee7874a3005501c0d477556c25c9d67e1f57245c7453da776b51cf011bffb2637608c09e3802706170706c69636174696f6e2f63626f72"
-//                 },
-//                 {
-//                     "meta": "0xff0a89c674ee7874a3007901573078383035386164376332326664633837383866653463623164616331356436653937363132373332342c3078633044343737353536633235433964363745316635373234354337343533444137373642353163662c3078364533376433346533356135664632663839366544396537364543343365373238616441316431382c3078326362323166623061326365626235373433346231613262383963383165356634396364343834612c3078616131646563656663326233326361363339306339373733653465636666653639613634346666372c3078363237613132636531663664343263393330356530336538336665303434653863336331613332632c3078626531346338663333323339646239363939343232623337663039616138366439336262386666362c307862616133653364643665656562663837616633396663333565656363646631323533376462353135011bffb2637608c0a00002706170706c69636174696f6e2f6a736f6e"
-//                 },
-//                 {
-//                     "meta": "0xff0a89c674ee7874a3005501aa1decefc2b32ca6390c9773e4ecffe69a644ff7011bffb2637608c09e3802706170706c69636174696f6e2f63626f72"
-//                 }
-//             ]
-//         }
-//     });
+  let res = json!({
+        "data": {
+            "metaV1S": [
+                {
+                    "meta": "0xff0a89c674ee7874a3005501c0d477556c25c9d67e1f57245c7453da776b51cf011bffb2637608c09e3802706170706c69636174696f6e2f63626f72"
+                },
+                {
+                    "meta": "0xff0a89c674ee7874a3005501aa1decefc2b32ca6390c9773e4ecffe69a644ff7011bffb2637608c09e3802706170706c69636174696f6e2f63626f72"
+                },
+                {
+                    "meta": "0xff0a89c674ee7874a30055008058ad7c22fdc8788fe4cb1dac15d6e976127324011bffb2637608c09e3802706170706c69636174696f6e2f63626f72"// removed
+                },
+                {
+                    "meta": "0xff0a89c674ee7874a3005501aa1decefc2b32ca6390c9773e4ecffe69a644ff7011bffb2637608c09e3802706170706c69636174696f6e2f63626f72"
+                },
+            ]
+        }
+    });
 
    let mut addresses: Vec<String> = Vec::new();
    let mut addresses2: Vec<String> = Vec::new();
@@ -102,6 +98,7 @@ if let Some(meta_v1s) = res["data"]["metaV1S"].as_array() {
             }
         }
     }
+    dbg!(&addresses);
         Ok(addresses)
 }
 
