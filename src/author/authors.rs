5fa4ac4d388a9da8e2f6ee9f9639d437035dc325
ninokeldishvili <ninokeldishvili@gmail.com>
2024-10-01 15:43:44 +0400

    Update subgraph url and get data from subgraph. remove hardcoded data

--- src/author/authors.rs
+++ src/author/authors.rs
@@ -1,18 +1,8 @@
-use anyhow::{Result, anyhow};
+use anyhow::{Result};
 use reqwest::Client;
-use serde_cbor::from_slice;
-use mockito::mock;
-use serde_json::json;
 use hex;
-use serde_json::{Value};
-use hex::FromHex;
 use serde::{Serialize, Deserialize};
-use rain_metadata::meta::{
-                             RainMetaDocumentV1Item, ContentType, ContentEncoding, ContentLanguage,
-                             KnownMagic
-                         };
-use rain_metadata::error::Error;
-
+use rain_metadata::meta::{RainMetaDocumentV1Item};
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Payload {
@@ -49,32 +39,11 @@ pub async fn get_authors() -> Result<Vec<String>> {
         }
     "#;
 
-    let url = "https://api.thegraph.com/subgraphs/name/ninokeldishvili/rain-metaboard";
-//     let res = get_data(url, query).await?;
+    let url = "https://api.goldsky.com/api/public/project_clycy1691w08b01z43sljeooc/subgraphs/metaBoard-arb-sepolia/1.0.0/gn";
+    let res = get_data(url, query).await?;
 
-
-  let res = json!({
-        "data": {
-            "metaV1S": [
-                {
-                    "meta": "0xff0a89c674ee7874a3005501c0d477556c25c9d67e1f57245c7453da776b51cf011bffb2637608c09e3802706170706c69636174696f6e2f63626f72"
-                },
-                {
-                    "meta": "0xff0a89c674ee7874a3005501aa1decefc2b32ca6390c9773e4ecffe69a644ff7011bffb2637608c09e3802706170706c69636174696f6e2f63626f72"
-                },
-                {
-                    "meta": "0xff0a89c674ee7874a30055008058ad7c22fdc8788fe4cb1dac15d6e976127324011bffb2637608c09e3802706170706c69636174696f6e2f63626f72"// removed
-                },
-                {
-                    "meta": "0xff0a89c674ee7874a3005501aa1decefc2b32ca6390c9773e4ecffe69a644ff7011bffb2637608c09e3802706170706c69636174696f6e2f63626f72"
-                },
-            ]
-        }
-    });
-
-   let mut addresses: Vec<String> = Vec::new();
-   let mut addresses2: Vec<String> = Vec::new();
-if let Some(meta_v1s) = res["data"]["metaV1S"].as_array() {
+    let mut addresses: Vec<String> = Vec::new();
+    if let Some(meta_v1s) = res["data"]["metaV1S"].as_array() {
         for item in meta_v1s {
             if let Some(meta_value) = item["meta"].as_str() {
                 let extracted_substring = &meta_value[18..]; // Remove rain meta magic_number
@@ -88,6 +57,7 @@ if let Some(meta_v1s) = res["data"]["metaV1S"].as_array() {
                             let address_str: String = hex::encode(payload);
                             let modified_address = format!("0x{}", &address_str[2..]);
                             addresses.push(modified_address);
+                            dbg!(&addresses);
                         }
                     }
                     Err(err) => {
@@ -98,85 +68,5 @@ if let Some(meta_v1s) = res["data"]["metaV1S"].as_array() {
             }
         }
     }
-    dbg!(&addresses);
-        Ok(addresses)
-}
-
-#[cfg(test)]
-pub mod test {
-    use super::*;
-
-#[tokio::test]
-async fn test_fetch_subgraph_dt() {
-    // Arrange
-    let _m = mock("POST", "/graphql")
-        .match_header("Content-Type", "application/json")
-        .with_body(r#"{"query":"test query"}"#)
-        .create();
-
-    // Act
-    let result = fetch_subgraph_dt(&mockito::server_url(), "test query").await;
-
-    // Assert
-    assert!(result.is_ok());
-    let json_value = result.unwrap();
-    assert_eq!(json_value, json!({ "test": "response" }));
-}
-
-#[tokio::test]
-async fn test_get_data() {
-    // Mocking fetch_subgraph_dt
-    let fetch_mock = mock("POST", "/graphql")
-        .match_header("Content-Type", "application/json")
-        .with_body(r#"{"data": { "key": "value" }}"#)
-        .create();
-
-    // Act
-    let result = get_data(&mockito::server_url(), "test query").await;
-    println!("{:?}", result);
-
-    // Assert
-    assert!(result.is_ok());
-    let json_value = result.unwrap();
-    assert_eq!(json_value, json!({ "key": "value" }));
-
-    // Assert that the expected request was made
-    fetch_mock.assert();
-}
-
-#[test]
-fn test_cbor_decode() {
-    // Test case for valid input
-    let input = "12345";
-    let result = cbor_decode(input);
-    println!("{:?}", result);
-
-    assert!(result.is_ok());
-    assert_eq!(result.unwrap(), vec![49, 50, 51, 52, 53]); // ASCII values of "12345"
-
-    // Test case for invalid input
-    let input = "abcde";
-    let result = cbor_decode(input);
-    assert!(result.is_err());
-}
-
-#[tokio::test]
-async fn test_get_authors() {
-    // Mocking get_data
-    let data_mock = mock("POST", "/graphql")
-        .match_header("Content-Type", "application/json")
-        .with_body(r#"{"data": { "metaV1S": [{ "meta": "12345" }] }}"#)
-        .create();
-
-    // Act
-    let result = get_authors().await;
-    println!("{:?}", result);
-
-    // Assert
-    assert!(result.is_ok());
-    assert_eq!(result.unwrap(), vec![49, 50, 51, 52, 53]); // ASCII values of "12345"
-
-    // Assert that the expected request was made
-    data_mock.assert();
-}
+    Ok(addresses)
 }
